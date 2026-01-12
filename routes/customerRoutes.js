e5cc89b30f5b90555ead6af9fb591f44c14c5145
Anandhu Prasad <anandhup059@gmail.com>
2026-01-12 15:26:18 +0530

    a

--- routes/customerRoutes.js
+++ routes/customerRoutes.js
@@ -1,6 +1,6 @@
 import express from "express";
 import { requireAuth } from "../middlewares/requireAuth.js";
-import { requireSuperAdmin } from "../middlewares/requireSuperAdmin.js";
+import { requireStaffOrAdmin } from "../middlewares/requireStaffOrAdmin.js";
 import {
   getCustomers,
   createCustomer,
@@ -11,9 +11,9 @@ import { requireBusiness } from "../middlewares/requireBusiness.js";
 
 const router = express.Router();
 
-router.get("/", requireAuth,requireBusiness, getCustomers);
-router.post("/", requireAuth, requireSuperAdmin,requireBusiness, createCustomer);
-router.patch("/:id", requireAuth, requireSuperAdmin,requireBusiness, updateCustomer);
-router.delete("/:id", requireAuth, requireSuperAdmin,requireBusiness, deleteCustomer);
+router.get("/", requireAuth, requireBusiness, getCustomers);
+router.post("/", requireAuth, requireStaffOrAdmin, requireBusiness, createCustomer);
+router.patch("/:id", requireAuth, requireStaffOrAdmin, requireBusiness, updateCustomer);
+router.delete("/:id", requireAuth, requireStaffOrAdmin, requireBusiness, deleteCustomer);
 
 export default router;
